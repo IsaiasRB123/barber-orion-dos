e79823da553a07b21fe0667b336118df5526ccad
M1GU3L10 <123miguelfps@gmail.com>
2024-03-22 11:09:33 -0500

    dwdw

--- main.js
+++ main.js
@@ -23,6 +23,10 @@ app.get('/Roles', (req, res) => {
   res.render('admin/Roles');
 });
 
+app.get('/Usuarios', (req, res) => {
+  res.render('admin/Usuarios');
+});
+
 app.get('/Categorias', (req, res) => {
   res.render('admin/Categorias');
 });

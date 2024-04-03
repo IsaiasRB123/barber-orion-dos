6cb4bd3d114b8dccdc786f25c12e93d59ee5b6fc
IsaiasRB123 <isaiwenhill@gmail.com>
2024-04-03 13:50:24 +0200

    mejora de ocultar

--- public/ADMIN/js/verContrasena.js
+++ public/ADMIN/js/verContrasena.js
@@ -1,19 +1,29 @@
-const pass = document.getElementById("pass"),
-        icon = document.querySelector(".bx");
 
-const passDos = document.getElementById("passDos"),
-
-        iconDos = document.querySelector(".bx");
-
-icon.addEventListener("click", e => {
-    if (pass.type === "password") {
-        pass.type = "text";
-        icon.classList.remove('bx bx-show-alt')
-        icon.classList.add('bx-hiden')
-    }else{
-        pass.type= "password"
-        icon.classList.add('bx bx-show-alt')
-        icon.classList.remove('bx-hiden')
+function togglePassword(fieldId, icon) {
+    const field = document.getElementById(fieldId);
+    const fieldType = field.getAttribute('type');
+    if (fieldType === 'password') {
+      field.setAttribute('type', 'text');
+      icon.classList.remove('bx-show-alt');
+      icon.classList.add('bx-hide');
+    } else {
+      field.setAttribute('type', 'password');
+      icon.classList.remove('bx-hide');
+      icon.classList.add('bx-show-alt');
     }
+  }
+// const passDos = document.getElementById("passDos"),
+//         iconDos = document.querySelector(".bx");
 
-})
+// iconDos.addEventListener("click", e => {
+//     if (pass.type === "password") {
+//         pass.type = "text";
+//         icon.classList.remove('bx bx-show-alt')
+//         icon.classList.add('bx-hiden')
+//     }else{
+//         pass.type= "password"
+//         icon.classList.add('bx bx-show-alt')
+//         icon.classList.remove('bx-hiden')
+//     }
+
+// })
